9fec6a8518fd82d84cca44f8d7ebe1166ee08c5a
kw <115936263+k-wlosek@users.noreply.github.com>
2023-05-12 15:31:14 +0200

    docs(test,log): add comments

--- src/logger.py
+++ src/logger.py
@@ -2,12 +2,14 @@ import logging
 
 
 class ShipRadarLogger(logging.Logger):
+    # Custom logging level, will output a lot
     VERBOSE = 5
     logging.VERBOSE = VERBOSE
     logging.Logger.VERBOSE = VERBOSE
 
     def __init__(self, name: str):
         super().__init__(name)
+        # TODO add file handler, add better formatter
         logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
         logging.addLevelName(self.VERBOSE, "VERBOSE")
 
--- test/test_reader.py
+++ test/test_reader.py
@@ -6,6 +6,10 @@ from src.reader import ShipRadarFilter, ShipRadarCSVReader
 
 class TestReadFilters(unittest.TestCase):
     def test_name(self):
+        """
+        Test for ship name filter
+        :return:
+        """
         filters = ShipRadarFilter('ship_name', 'Test')
         reader = ShipRadarCSVReader('data.csv')
         self.assertEqual(reader.parse(filters), [{'ship': 'Test', 'date': '202301011200', 'location': 'x0y0'},
@@ -14,6 +18,10 @@ class TestReadFilters(unittest.TestCase):
                          "Failed test: CSV filter ship name")
 
     def test_time(self):
+        """
+        Test for time filter
+        :return:
+        """
         filters = ShipRadarFilter('date', '20230101', 721)
         reader = ShipRadarCSVReader('data.csv')
         self.assertEqual(reader.parse(filters), [{'ship': 'Test', 'date': '202301011200', 'location': 'x0y0'},
@@ -22,6 +30,10 @@ class TestReadFilters(unittest.TestCase):
                          "Failed test: CSV filter time")
 
     def test_coords(self):
+        """
+        Test for coords filter
+        :return:
+        """
         filters = ShipRadarFilter('coords', 0, 0, 300, 300)
         reader = ShipRadarCSVReader('data.csv')
         self.assertEqual(reader.parse(filters), [{'ship': 'Test', 'date': '202301011200', 'location': 'x0y0'},
@@ -30,11 +42,19 @@ class TestReadFilters(unittest.TestCase):
                          "Failed test: CSV filter coords")
 
     def test_nonexistent_filter(self):
+        """
+        Test for nonexistent filter
+        :return:
+        """
         with self.assertRaises(err.ShipRadarFilterError) as exc:
             filters = ShipRadarFilter('none', 0)
         self.assertTrue('Filter type does not exist' in str(exc.exception))
 
     def test_null_result_filter(self):
+        """
+        Test for filter that returns no results
+        :return:
+        """
         filters = ShipRadarFilter('ship_name', 'DefinitelyNotTest')
         reader = ShipRadarCSVReader('data.csv')
         with self.assertRaises(err.ShipRadarFilterError) as exc:
@@ -45,6 +65,10 @@ class TestReadFilters(unittest.TestCase):
 # TODO write tests for invalid CSV file
 class TestCSVReader(unittest.TestCase):
     def test_nonexistent_file(self):
+        """
+        Test for nonexistent file
+        :return:
+        """
         filters = ShipRadarFilter('ship_name', 'Test')
         reader = ShipRadarCSVReader('not_data.csv')
         with self.assertRaises(err.ShipRadarImportError) as exc:
