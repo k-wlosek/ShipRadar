1fa349110130588a31b93fa235deb3959873885e
kw <115936263+k-wlosek@users.noreply.github.com>
2023-05-15 13:06:03 +0200

    fix(gui): fix message if dates are invalid

--- gui/Filters.py
+++ gui/Filters.py
@@ -36,27 +36,82 @@ class DateTimeFilter(flet.UserControl):
         self.logger = ShipRadarLogger("DateTimeFilter")
         self.page = page
         self.page.title = "Filter: Date and time"
-        self.datetime_field = DatetimeField(page=self.page)
+        self.from_datetime_field = DatetimeField(page=self.page)
+        self.till_datetime_field = DatetimeField(page=self.page)
 
     def build(self):
         self.layout = flet.Column(
             [
                 flet.Text("Filter by date and time"),
-                self.datetime_field,
+                self.from_datetime_field,
+                self.till_datetime_field,
+                flet.ElevatedButton(text="Submit", on_click=self.submit)
+            ]
+        )
+        return self.layout
+
+    def submit(self, e) -> None:
+        self.logger.debug(f"{self.from_datetime_field.value=} {self.till_datetime_field.value=}")
+        # Check if either value is a list, meaning there's a problem with said value
+        if isinstance(self.from_datetime_field.value, list):
+            # If all values in list are str then it's a problem with the date
+            self.logger.debug("Problem with from date, probably invalid date")
+            if all(isinstance(x, str) for x in self.from_datetime_field.value):
+                self.page.snack_bar = flet.SnackBar(content=flet.Text("Invalid from date"))
+                self.page.snack_bar.open = True
+                self.page.update()
+                return None
+        if isinstance(self.till_datetime_field.value, list):
+            self.logger.debug("Problem with till date, probably invalid date")
+            if all(isinstance(x, str) for x in self.till_datetime_field.value):
+                self.page.snack_bar = flet.SnackBar(content=flet.Text("Invalid till date"))
+                self.page.snack_bar.open = True
+                self.page.update()
+                return None
+        if isinstance(self.from_datetime_field.value, datetime) and \
+                isinstance(self.till_datetime_field.value, datetime):
+            # All fields are filled
+            self.filter = ShipRadarFilter('date', self.from_datetime_field.value, self.till_datetime_field.value)
+            self.page.session.set('datetime_filter', self.filter)
+            self.page.go("/filters")
+        else:
+            # Not all fields are filled
+            self.page.snack_bar = flet.SnackBar(content=flet.Text("Please fill all fields"))
+            self.page.snack_bar.open = True
+            self.page.update()
+            return None
+
+
+class LocationFilter(flet.UserControl):
+    def __init__(self, page: flet.Page):
+        super().__init__()
+        self.logger = ShipRadarLogger("LocationFilter")
+        self.page = page
+        self.page.title = "Filter: Location"
+        self.x1 = flet.TextField(label="x1", keyboard_type=flet.KeyboardType.NUMBER)
+        self.y1 = flet.TextField(label="y1", keyboard_type=flet.KeyboardType.NUMBER)
+        self.x2 = flet.TextField(label="x2", keyboard_type=flet.KeyboardType.NUMBER)
+        self.y2 = flet.TextField(label="y2", keyboard_type=flet.KeyboardType.NUMBER)
+
+    def build(self):
+        self.layout = flet.Column(
+            [
+                flet.Text("Filter by ship name"),
+                flet.Container(
+                    content=flet.GridView(
+                        controls=[
+                            self.x1, self.y1, self.x2, self.y2
+                        ],
+                        spacing=5, expand=1, run_spacing=5
+                    )
+                ),
                 flet.ElevatedButton(text="Submit", on_click=self.submit)
             ]
         )
         return self.layout
 
     def submit(self, e):
-        self.logger.debug(self.datetime_field.value)
-        if isinstance(self.datetime_field.value, datetime):
-            # All fields are filled
-            self.filter = ShipRadarFilter('ship_date', self.datetime_field.value)
-            self.filter = ShipRadarFilter('ship_datetime', self.datetime_field.value)
-            self.page.session.set('datetime_filter', self.filter)
-            self.page.go("/filters")
-        else:
-            # Not all fields are filled
-            self.page.show_snackbar("Please fill all fields")
-
+        self.logger.debug(self.text_field.value)
+        self.filter = ShipRadarFilter('ship_name', self.text_field.value)
+        self.page.session.set('name_filter', self.filter)
+        self.page.go("/filters")
